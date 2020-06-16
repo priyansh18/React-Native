28d04d9f7171950873b34c89779524b6a48885bf
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-16 18:21:22 +0530

    Alerts

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import {
-  StyleSheet,
-  SafeAreaView,
-  Button,
-} from "react-native";
+import { StyleSheet, SafeAreaView, Button, Alert } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Button title="Click Me" onPress={()=>console.log("Button Tapped")}/>
+      <Button
+        title="Click Me"
+        onPress={() =>
+          Alert.alert("My title", "My message", [
+            { text: "Yes", onPress: console.log("Yes") },
+            { text: "No", onPress: console.log("No") },
+          ])
+        }
+      />
     </SafeAreaView>
   );
 }
