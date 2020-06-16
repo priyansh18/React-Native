23da809fb519c14044f3640180e991cc04f8a5aa
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-16 19:13:30 +0530

    Detecting Orientation Changes

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -9,13 +9,22 @@ import {
   View,
   Dimensions,
 } from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
-  console.log(Dimensions.get('screen'))
+  const { landscape } = useDeviceOrientation();
+
   return (
     <SafeAreaView style={styles.container}>
       <View
-        style={{ backgroundColor: "dodgerblue", width: "50%", height: 70 }}
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: landscape ? "100%" : "30%",
+        }}
       ></View>
     </SafeAreaView>
   );
