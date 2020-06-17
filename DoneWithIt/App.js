e78de1321559dcf3f6a6acaa7d5d02fe30dc1996
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-17 12:43:17 +0530

    Building a view Image Screen

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { View } from "react-native";
 import WelcomeScreen from './app/screens/welcomeScreen';
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
   return (
-    <WelcomeScreen/>
+    <ViewImageScreen/>
   );
 }
