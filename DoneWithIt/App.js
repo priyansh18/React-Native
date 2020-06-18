f7c4f89342973c9e7bcc2810224aa3c9b74edf70
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-18 12:35:50 +0530

    FlatList

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -4,9 +4,10 @@ import WelcomeScreen from "./app/screens/welcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/components/ListingDetailsScreen";
+import MessagesScreen from './app/screens/MessagesScreen'
 
 export default function App() {
   return (
-    <ViewImageScreen/>
+    <MessagesScreen/>
   );
 }
---
+++ DoneWithIt/app/screens/MessagesScreen.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { FlatList } from "react-native";
+import ListItem from "./../components/ListItem";
+
+const messages = [
+  {
+    id: 1,
+    title: "T1",
+    description: "D1",
+    image: require("../assets/priyansh.jpg"),
+  },
+  {
+    id: 2,
+    title: "T2",
+    description: "D2",
+    image: require("../assets/priyansh.jpg"),
+  },
+];
+
+function MessagesScreen(props) {
+  return (
+    <FlatList
+      data={messages}
+      keyExtractor={(messages) => messages.id.toString()}
+      renderItem={({ item }) => (
+        <ListItem
+          title={item.title}
+          subTitle={item.description}
+          image={item.image}
+        />
+      )}
+    />
+  );
+}
+
+
+export default MessagesScreen;
