a864c14e0fc59a27aa4a83a3792cc7181d131035
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-18 13:28:33 +0530

    Handling Swipe Action

--- DoneWithIt/app/screens/MessagesScreen.js
+++ DoneWithIt/app/screens/MessagesScreen.js
@@ -3,6 +3,7 @@ import { FlatList, StyleSheet, View } from "react-native";
 import ListItem from "./../components/ListItem";
 import Screen from "./../components/Screen";
 import ListItemSeprator from "../components/ListItemSeprator";
+import ListItemDeleteAction from './../components/ListItemDeleteAction';
 
 const messages = [
   {
@@ -30,7 +31,8 @@ function MessagesScreen(props) {
             title={item.title}
             subTitle={item.description}
             image={item.image}
-            onPress={()=>console.log("Message Selected",item)}
+            onPress={() => console.log("Message Selected", item)}
+            renderRightActions={ListItemDeleteAction}
           />
         )}
         ItemSeparatorComponent={ListItemSeprator}
