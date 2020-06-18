afd58025460fe37a0df836102f99a51285bc28f8
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-18 15:46:35 +0530

    Build Icon Component

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -4,10 +4,14 @@ import WelcomeScreen from "./app/screens/welcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/components/ListingDetailsScreen";
-import MessagesScreen from './app/screens/MessagesScreen'
+import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from './app/components/Icon';
 
 export default function App() {
   return (
-    <MessagesScreen/>
+    <Screen>
+      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+    </Screen>
   );
 }
