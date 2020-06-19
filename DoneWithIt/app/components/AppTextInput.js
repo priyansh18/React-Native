0e39c1539cdd3954fb676bb05ee241673cf140d2
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-19 11:33:04 +0530

    Extracting the default styles

--- DoneWithIt/app/components/AppTextInput.js
+++ DoneWithIt/app/components/AppTextInput.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, TextInput, Platform, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import colors from "../config/colors";
+import defaultStyles from "../config/styles";
 
 function AppTextInput({ icon, ...otherProps }) {
   return (
@@ -14,7 +15,7 @@ function AppTextInput({ icon, ...otherProps }) {
           style={styles.icon}
         />
       )}
-      <TextInput style={styles.textInput} {...otherProps} />
+      <TextInput style={defaultStyles.text} {...otherProps} />
     </View>
   );
 }
@@ -27,11 +28,6 @@ const styles = StyleSheet.create({
     width: "100%",
     padding: 15,
     marginVertical: 10,
-  },
-  textInput: {
-    color: colors.dark,
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
   },
   icon: {
     marginRight: 10,
